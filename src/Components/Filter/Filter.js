94476708f1d0b8e277bf2c5bd60739a6c781c260
mmarakesh <marinamakarova749@gmail.com>
2024-02-20 00:57:15 +0100

    add css selected category

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,14 +1,16 @@
-import { useDispatch } from "react-redux";
-import { filterCategory } from "../../redux/productSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { filterCategory, getSelectedCategory } from "../../redux/productSlice";
 import { useTranslation } from "react-i18next";
 
 const Filter = ({category}) => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
+    const selectedCategory = useSelector(getSelectedCategory)
 
     return (
         <div className="cont products">
-            <p onClick={() => {dispatch(filterCategory(category))}} className="change">
+            <p onClick={() => {dispatch(filterCategory(category))}} 
+            className={selectedCategory === category ? 'categoryButtonSelected categoryButton' : 'categoryButton'}>
                 {t(category)}
             </p>
         </div>
